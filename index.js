b2984e0bcd86347158e4fcf65f5b4677160101f4
Adnan Adil <adnanadil529@gmail.com>
2022-11-20 18:13:19 +0300

    my original 1.0

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ io.on("connection", (socket) => {
 
   socket.on("send_message", (data) => {
     // socket.to(data.room).emit("receive_message", data);
-    // socket.broadcast.emit("receive_message", data);
-    io.broadcast.emit("receive_message", data);
+    socket.broadcast.emit("receive_message", data);
+    // io.broadcast.emit("receive_message", data);
   });
 });
 

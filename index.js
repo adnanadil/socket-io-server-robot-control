3483bee17dfb5905fa8627d69252494f528a94da
Adnan Adil <adnanadil529@gmail.com>
2022-12-26 09:03:49 +0300

    first commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
+// const { Server } = require('socket.io')(http, { wsEngine: 'ws' });
 const cors = require("cors");
 
 app.use(cors());
@@ -30,6 +31,12 @@ io.on("connection", (socket) => {
   });
 
   socket.on("send_message", (data) => {
+    // socket.to(data.room).emit("receive_message", data);
+    socket.broadcast.emit("receive_message", data);
+    // io.broadcast.emit("receive_message", data);
+  });
+
+  socket.on("send_this", (data) => {
     // socket.to(data.room).emit("receive_message", data);
     socket.broadcast.emit("receive_message", data);
     // io.broadcast.emit("receive_message", data);

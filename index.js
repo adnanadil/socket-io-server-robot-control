137d6e7895faa83b9a183f9562828738f41d0f77
Adnan Adil <adnanadil529@gmail.com>
2022-12-27 03:01:14 +0300

    changing cors again

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ const io = new Server(server, {
     origin: "*",
     methods: ["GET", "POST"],
     transports: ["websocket", "polling"],
-    credentials: false,
-    // credentials: true,
+    credentials: true,
    },
     allowEIO3: true,
 });
